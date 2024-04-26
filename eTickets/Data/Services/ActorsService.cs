e30e8625eeaa2499feee1ade2b392c647726726d
Himesh <himeshmauurya@gmail.com>
2024-04-26 22:34:24 +0530

    etickets

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,48 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : EntityBaseRepository<Actor>, IActorsService
+    {
+        public ActorsService(AppDbContext context) : base(context) { }
+    }
+    /*public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var result =await _context.Actors.ToListAsync();
+            return result;
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }*/
+}
